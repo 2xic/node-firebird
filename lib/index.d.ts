6de5b236149bf215b39248c14d10292870116ade
Brage <5640782+2xic@users.noreply.github.com>
2022-03-29 13:32:33 +0200

    Fixed type annotation

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Marco Warm <https://github.com/MarcusCalidus>
 
 declare module 'node-firebird' {
-    type DatabaseCallback = (err: any, db: Database) => void;
+    type DatabaseCallback = (err: any, db: Database | undefined) => void;
     type TransactionCallback = (err: any, transaction: Transaction) => void;
     type QueryCallback = (err: any, result: any[]) => void;
     type SimpleCallback = (err: any) => void;
